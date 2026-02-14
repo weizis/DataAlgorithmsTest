061a58dd2bb264b52c9b9c1be875e31908cb34c7
weizis <nastyamik2019@mail.ru>
2026-02-15 00:43:05 +0300

    Initializing the MSTest test project for DataAlgorithms

---
+++ SearchingTests.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using DataAlgorithms.Models;
+using DataAlgorithms.Comparers;
+using DataAlgorithms.Searching;
+
+namespace DataAlgorithmsTest
+{
+    [TestClass]
+    public class SearchingTests
+    {
+        [TestMethod]
+        public void BinarySearch_FindsCorrectIndex()
+        {
+            var people = new List<Person>
+            {
+                new Person { LastName = "Ivanov", Age = 25 },
+                new Person { LastName = "Petrov", Age = 30 },
+                new Person { LastName = "Sidorov", Age = 40 }
+            };
+
+            var comparer = new PersonLastNameComparer();
+            var personToFind = new Person { LastName = "Petrov" };
+
+            int index = BinarySearch.Search(people, personToFind, comparer);
+
+            Assert.AreEqual(1, index); 
+        }
+        [TestMethod]
+        public void BinarySearch_ElementNotFound_ReturnsMinusOne()
+        {
+            var people = new List<Person>
+            {
+                new Person { LastName = "Ivanov", Age = 25 },
+                new Person { LastName = "Petrov", Age = 30 }
+            };
+            var comparer = new PersonLastNameComparer();
+            var personToFind = new Person { LastName = "Sidorov" };
+
+            int index = BinarySearch.Search(people, personToFind, comparer);
+
+            Assert.AreEqual(-1, index);
+        }
+
+        [TestMethod]
+        public void LinearSearch_FindsCorrectIndex()
+        {
+            var people = new List<Person>
+            {
+                new Person { LastName = "Ivanov", Age = 25 },
+                new Person { LastName = "Petrov", Age = 30 },
+                new Person { LastName = "Sidorov", Age = 40 }
+            };
+
+            int index = LinearSearch.Search(people, p => p.LastName == "Sidorov");
+
+            Assert.AreEqual(2, index); 
+        }
+        [TestMethod]
+        public void LinearSearch_EmptyList_ReturnsMinusOne()
+        {
+            var people = new List<Person>();
+
+            int index = LinearSearch.Search(people, p => p.LastName == "Ivanov");
+
+            Assert.AreEqual(-1, index);
+        }
+
+    }
+}
---
+++ SortingTests.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using DataAlgorithms.Models;
+using DataAlgorithms.Comparers;
+using DataAlgorithms.Sorting;
+
+namespace DataAlgorithmsTest
+{
+    [TestClass] 
+    public class SortingTests
+    {
+        [TestMethod] 
+        public void MergeSort_SortsByLastNameCorrectly()
+        {
+            var people = new List<Person>
+            {
+                new Person { LastName = "Petrov", Age = 30 },
+                new Person { LastName = "Ivanov", Age = 25 },
+                new Person { LastName = "Sidorov", Age = 40 }
+            };
+
+            var comparer = new PersonLastNameComparer();
+
+            var result = MergeSort.Sort(people, comparer);
+
+            Assert.AreEqual("Ivanov", result[0].LastName);
+            Assert.AreEqual("Petrov", result[1].LastName);
+            Assert.AreEqual("Sidorov", result[2].LastName);
+        }
+        [TestMethod]
+        public void MergeSort_EmptyList_ReturnsEmpty()
+        {
+            var people = new List<Person>();
+            var comparer = new PersonLastNameComparer();
+
+            var result = MergeSort.Sort(people, comparer);
+
+            Assert.AreEqual(0, result.Count);
+        }
+
+        [TestMethod]
+        public void QuickSort_SortsByLastNameCorrectly()
+        {
+            var people = new List<Person>
+            {
+                new Person { LastName = "Petrov", Age = 30 },
+                new Person { LastName = "Ivanov", Age = 25 },
+                new Person { LastName = "Sidorov", Age = 40 }
+            };
+
+            var comparer = new PersonLastNameComparer();
+
+            QuickSort.Sort(people, comparer); 
+
+            Assert.AreEqual("Ivanov", people[0].LastName);
+            Assert.AreEqual("Petrov", people[1].LastName);
+            Assert.AreEqual("Sidorov", people[2].LastName);
+        }
+        [TestMethod]
+        public void QuickSort_OneElement_ListUnchanged()
+        {
+            var people = new List<Person> { new Person { LastName = "Ivanov", Age = 25 } };
+            var comparer = new PersonLastNameComparer();
+
+            QuickSort.Sort(people, comparer);
+
+            Assert.AreEqual(1, people.Count);
+            Assert.AreEqual("Ivanov", people[0].LastName);
+        }
+
+        [TestMethod]
+        public void BubbleSort_SortsByLastNameCorrectly()
+        {
+            var people = new List<Person>
+            {
+                new Person { LastName = "Petrov", Age = 30 },
+                new Person { LastName = "Ivanov", Age = 25 },
+                new Person { LastName = "Sidorov", Age = 40 }
+            };
+
+            var comparer = new PersonLastNameComparer();
+
+            BubbleSort.Sort(people, comparer); 
+
+            Assert.AreEqual("Ivanov", people[0].LastName);
+            Assert.AreEqual("Petrov", people[1].LastName);
+            Assert.AreEqual("Sidorov", people[2].LastName);
+        }
+        [TestMethod]
+        public void BubbleSort_RepeatedLastNames_SortsCorrectly()
+        {
+            var people = new List<Person>
+            {
+                new Person { LastName = "Petrov", Age = 30 },
+                new Person { LastName = "Petrov", Age = 25 },
+                new Person { LastName = "Ivanov", Age = 40 }
+            };
+            var comparer = new PersonLastNameComparer();
+
+            BubbleSort.Sort(people, comparer);
+
+            Assert.AreEqual("Ivanov", people[0].LastName);
+            Assert.AreEqual("Petrov", people[1].LastName);
+            Assert.AreEqual("Petrov", people[2].LastName);
+        }
+
+
+
+
+    }
+}
